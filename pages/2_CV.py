198fe87cdac6422ee3c72c3255bc70b888ffb726
Quentin Bernardin <quentin-be@hotmail.fr>
2024-05-30 10:11:26 +0200

    Update 2_CV.py

--- pages/2_CV.py
+++ pages/2_CV.py
@@ -2,6 +2,8 @@ import streamlit as st
 import base64
 from constant import *
 from PIL import Image
+from streamlit_pdf_viewer import pdf_viewer
+
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -20,9 +22,10 @@ st.markdown(f'<h1 style="text-align:center;background-color:#90ADC6;font-size:60
             unsafe_allow_html=True)
 st.write("")
 
-with open("images/resume.pdf","rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="90%" height="860px" type="application/pdf"></iframe>'
-      st.markdown(f'<div style="text-align: center;">{pdf_display}</div>', unsafe_allow_html=True)    
+# with open("images/resume.pdf","rb") as f:
+#       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="90%" height="860px" type="application/pdf"></iframe>'
+#       st.markdown(f'<div style="text-align: center;">{pdf_display}</div>', unsafe_allow_html=True)    
 
+pdf_viewer("images/resume.pdf")
   
