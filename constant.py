525699758838a4099a8e59534e74a0b3b3180f4f
Quentin Bernardin <quentin-be@hotmail.fr>
2024-06-26 21:25:24 +0200

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,8 +1,8 @@
 info = {
    "Name": "Quentin",
    "Full_Name": "Quentin Bernardin",
-   "Intro": "Enthousiaste de la data, machine learning comme engineering !",
-   "About":"""Je me présente, Quentin, passionné par l'utilisation des données et de la technologie pour générer des insights et des solutions significatives en entreprise. Je possède également un intérêt pour l'ingénierie des données, notamment la conception et la gestion de pipelines ETL, ainsi que pour la data science, où j'utilise des algorithmes pour répondre à diverses problématiques à l'aide des données.""",
+   "Intro": "Enthousiaste de la data science !",
+   "About":"""Je me présente, Quentin, passionné par l'utilisation des données et de la technologie pour générer des insights et des solutions significatives en entreprise par le biais d'algorithme ou de deep learning. Je possède également un intérêt pour l'ingénierie des données, notamment la conception et la gestion de pipelines ETL, ainsi que pour le traitement et l'interprétation de language naturel via LLM.""",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
    "City":"Lyon, France",
    "Email": "quentin-be@hotmail.fr"
